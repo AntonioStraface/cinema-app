b9d12a5ff35ecbcb0a90542818559e68d913bb11
DESKTOP-3CV1TKT\Antonio <strafaceant@gmail.com>
2019-03-30 19:11:17 +0100

    animate button on click

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -11,13 +11,15 @@ class Header extends React.Component { // eslint-disable-line react/prefer-state
     const { handlerOpenSearch, searchStatus } = this.props;
 
     let formClassName = searchStatus !== 'opened' ? 'header__search-form' : 'header__search-form header__search-form--opened';
+    let buttonType = searchStatus !== 'opened' ? 'submit' : 'reset';  
+
 
     return (
       <header className="header">
         <a href="/" className="header__logo">LOGO</a>
         <form className={formClassName} method="get" action="" onSubmit={handlerOpenSearch}>
           <input type="text" placeholder="Search your movie here" />
-          <button type="submit">lmlwemrflwme</button>
+          <button type={buttonType}>c</button>
         </form>
       </header>
     );
